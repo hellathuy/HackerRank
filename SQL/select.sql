746b14de77f8205b07efd5e4e8bbc1a837c073ec
Thuy Le <thuytien.ngoc.le@gmail.com>
2018-06-27 19:40:06 -0400

    Update select.sql

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,4 +1,5 @@
-/* Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA. */
+/* Query all columns for all American cities in CITY with populations larger than 100000. */
 
-SELECT * FROM CITY
-WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+SELECT * 
+FROM CITY
+  WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
