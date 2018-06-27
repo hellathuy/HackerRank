2cabaa4ad97976d4b67d18f17e16b1621ce62d5d
Thuy Le <thuytien.ngoc.le@gmail.com>
2018-06-27 19:49:20 -0400

    Update japanese_cities.sql

--- SQL/easy/japanese_cities.sql
+++ SQL/easy/japanese_cities.sql
@@ -1,4 +1,4 @@
-/* Query the names of all the Japanese cities in the CITY table. */
+/* https://www.hackerrank.com/challenges/japanese-cities-name/problem */
 
 SELECT NAME FROM CITY 
   WHERE COUNTRYCODE = 'JPN';
